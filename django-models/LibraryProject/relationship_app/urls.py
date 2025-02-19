5e7f85472f51bcc4f430bfc59f062a4c8083b0d1
Precious Nnorom <nnorompreciousakachukwu@gmail.com>
2025-02-20 00:39:36 +0100

    Implemented class-based view for library details and added template

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import LibraryDetailView
+
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
