498fe1719a1276427f6b043091f84758918b7be9
Precious Nnorom <nnorompreciousakachukwu@gmail.com>
2025-02-20 01:55:31 +0100

    Implemented Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView  # Import both views
+from .views import admin_view, librarian_view, member_view
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,7 +10,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-    
+
+ path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
     # URL pattern for the function-based view (list_books)
     path('books/', list_books, name='list_books'),
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from .models import Book
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login  
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 
 def register(request):
     if request.method == 'POST':
@@ -30,3 +31,28 @@ class LibraryDetailView(DetailView):
     model = Library  # Specify the model
     template_name = 'relationship_app/library_detail.html'  # Specify the template
     context_object_name = 'library'
+
+    # Helper functions to check roles
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
