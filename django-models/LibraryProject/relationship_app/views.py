47c75d94a288067d7a234e91246e1b2f71dd516f
Precious Nnorom <nnorompreciousakachukwu@gmail.com>
2025-02-20 01:12:43 +0100

    Fixed LibraryDetailView to use DetailView with correct import and updated template path

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,16 @@
 from django.views.generic import DetailView
+from .models import Library
 from django.shortcuts import render
 from .models import Book
-from .models import Library
+
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
+
 class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'  # Correct template path
-    context_object_name = 'library'  # Optional: Makes 'library' available in the template
+    model = Library  # Specify the model
+    template_name = 'relationship_app/library_detail.html'  # Specify the template
+    context_object_name = 'library'
