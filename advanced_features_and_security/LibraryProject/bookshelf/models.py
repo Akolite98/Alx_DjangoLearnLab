d5f6f9b82e5854bfd72d5d74bf697fd85305a0a4
Precious Nnorom <nnorompreciousakachukwu@gmail.com>
2025-02-26 16:23:42 +0100

    feat: register CustomUser in admin with CustomUserAdmin for managing additional fields

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 from django.db import models
 
 
@@ -33,3 +34,10 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
