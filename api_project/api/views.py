4057fb45b065b6eeac4354a1ff3db9d64a2916b0
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-05 10:20:18 +0100

    fix(api): ensure BookList view correctly extends ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,8 @@
 # api/views.py
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()  # Retrieve all books
-    serializer_class = BookSerializer  # Use the BookSerializer for serialization
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()  # Retrieve all books from the database
+    serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
