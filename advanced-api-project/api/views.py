cc124a2383b128b40b31f5632838810d8fde5fd7
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-13 10:57:09 +0100

    feat: add generic views for CRUD operations on books

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListAPIView):
+    """Retrieve a list of all books."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    """Retrieve details of a single book by ID."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    """Create a new book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    """Update an existing book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
+    """Delete a book by ID."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
