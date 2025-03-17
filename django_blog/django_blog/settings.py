01181eb7b5911d45ea12e19db528339410a5359f
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-17 13:08:28 +0100

    chore: Set up static and template directories

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "blog/static"]
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / "blog/templates"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
