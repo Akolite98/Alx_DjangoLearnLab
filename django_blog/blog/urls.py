e383a58d3e4db0e663bd37d7bb9c3b5a3c258e55
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-18 13:43:22 +0100

    fix: imported the right modules in views.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
+
 from .views import (
     PostListView,
     PostDetailView,
@@ -13,4 +15,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+     path('post/<int:post_id>/comment/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Post
+from .models import Comment, Post
+from .forms import CommentForm
 
 # Existing PostListView
 class PostListView(ListView):
@@ -50,3 +51,39 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set the logged-in user as the author
+        form.instance.post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()  # Redirect to the post after adding a comment
+
+
+class CommentUpdateView(UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def get_queryset(self):
+        return Comment.objects.filter(author=self.request.user)  # Ensure only the author can edit
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    template_name = "blog/comment_confirm_delete.html"
+
+    def get_queryset(self):
+        return Comment.objects.filter(author=self.request.user)  # Ensure only the author can delete
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
