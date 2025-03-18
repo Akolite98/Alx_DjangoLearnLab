9896d9f63d505c44f47326572f6c55312005a19e
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-18 14:08:22 +0100

    fix: made a fix

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Comment, Post
+from .models import Post, Comment
 from .forms import CommentForm
 
 # Existing PostListView
