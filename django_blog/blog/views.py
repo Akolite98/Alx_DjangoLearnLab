9e7bca0c40d367f31ba7e4e0ff3e621e52441f60
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-18 12:44:42 +0100

    feat: Define URL patterns for CRUD operations on posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,15 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
+from django.urls import reverse_lazy
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-created_at']
+    ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
     model = Post
@@ -27,6 +28,10 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
     def test_func(self):
         post = self.get_object()
