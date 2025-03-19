445c7ff0eb4e91d04f90e605e9ba051663a04928
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-19 10:45:55 +0100

    feat:added pagination

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,14 +2,18 @@ from taggit.forms import TagField
 from django import forms
 from .models import Post
 from .models import Comment 
+from taggit.forms import TagWidget 
+# from .widgets import TagWidget  # Import from widgets.py if custom
+
 from django.forms import widgets  
 
 class PostForm(forms.ModelForm):
-    tags = TagField(required=False)  # Allow users to add comma-separated tags
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  # Apply TagWidget to the tags field
+        }
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
