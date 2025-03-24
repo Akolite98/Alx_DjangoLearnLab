afc6295872a91039cda327f7156ffe018cc00124
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-24 20:05:40 +0100

    fix: changed my settings from User to CustomeUser

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,37 +1,32 @@
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework import status
 from django.shortcuts import get_object_or_404
-from .models import CustomUser
+from .models import CustomUser 
+from .serializers import UserSerializer
 
-@api_view(['POST'])
-def follow_user(request, user_id):
-    target_user = get_object_or_404(CustomUser, id=user_id)
-    
-    if request.user == target_user:
-        return Response(
-            {"error": "You cannot follow yourself."},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-    
-    request.user.following.add(target_user)
-    return Response(
-        {"message": f"You are now following {target_user.username}"},
-        status=status.HTTP_200_OK
-    )
+class FollowUnfollowView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()  # Using CustomUser as required
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-@api_view(['POST'])
-def unfollow_user(request, user_id):
-    target_user = get_object_or_404(CustomUser, id=user_id)
-    
-    if request.user == target_user:
-        return Response(
-            {"error": "You cannot unfollow yourself."},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-    
-    request.user.following.remove(target_user)
-    return Response(
-        {"message": f"You have unfollowed {target_user.username}"},
-        status=status.HTTP_200_OK
-    )
+    def post(self, request, *args, **kwargs):
+        target_user = get_object_or_404(CustomUser, pk=kwargs.get('pk'))  # Changed here too
+        
+        if request.user == target_user:
+            return Response(
+                {"error": "You cannot follow/unfollow yourself."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        if request.user.following.filter(pk=target_user.pk).exists():
+            request.user.following.remove(target_user)
+            return Response(
+                {"message": f"You have unfollowed {target_user.username}"},
+                status=status.HTTP_200_OK
+            )
+        else:
+            request.user.following.add(target_user)
+            return Response(
+                {"message": f"You are now following {target_user.username}"},
+                status=status.HTTP_200_OK
+            )
