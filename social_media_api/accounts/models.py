d9e58f007f53406f36cbeaff9c735f3b3352bebe
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-24 17:57:30 +0100

    feat: implement custom user model with bio, profile_picture and followers

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(_('bio'), blank=True)
+    profile_picture = models.ImageField(
+        _('profile picture'), 
+        upload_to='profile_pics/', 
+        blank=True, 
+        null=True
+    )
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        blank=True,
+        related_name='following'
+    )
+
+    def __str__(self):
+        return self.username
