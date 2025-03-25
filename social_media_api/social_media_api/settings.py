22a26047a736b8f238297f8ded71af8f7f1a4fcf
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-25 15:49:35 +0100

    feat: add required security headers and settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -100,6 +100,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'akolite',
+        'PASSWORD': 'akolite98',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
