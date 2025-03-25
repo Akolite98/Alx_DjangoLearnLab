5a6988d0c0763dd557bc48d82d88580264168500
Akolite <nnorompreciousakachukwu@gmail.com>
2025-03-25 15:11:56 +0100

    feat: add notifications for follow actions

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import CustomUser 
 from .serializers import UserSerializer
+from notifications.models import Notification
 
 class FollowUnfollowView(generics.GenericAPIView):
     queryset = CustomUser.objects.all()  # Using CustomUser as required
@@ -26,6 +27,37 @@ class FollowUnfollowView(generics.GenericAPIView):
             )
         else:
             request.user.following.add(target_user)
+            return Response(
+                {"message": f"You are now following {target_user.username}"},
+                status=status.HTTP_200_OK
+            )
+        
+class FollowUnfollowView(generics.GenericAPIView):
+    # ... existing code ...
+    def post(self, request, *args, **kwargs):
+        target_user = get_object_or_404(CustomUser, pk=kwargs.get('pk'))
+        
+        if request.user == target_user:
+            return Response(
+                {"error": "You cannot follow/unfollow yourself."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        if request.user.following.filter(pk=target_user.pk).exists():
+            request.user.following.remove(target_user)
+            return Response(
+                {"message": f"You have unfollowed {target_user.username}"},
+                status=status.HTTP_200_OK
+            )
+        else:
+            request.user.following.add(target_user)
+            # Create notification
+            Notification.objects.create(
+                recipient=target_user,
+                actor=request.user,
+                verb="started following you",
+                target=request.user
+            )
             return Response(
                 {"message": f"You are now following {target_user.username}"},
                 status=status.HTTP_200_OK
